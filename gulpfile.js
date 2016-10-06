185f8fc8034b40d713da09f342c7234cb82fb01f
Jacob <jacob.voronin@gmail.com>
2016-10-06 11:35:44 -0700

    Adding server file

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')({
+    rename: {
+        'gulp-webserver':  'webserver'
+    }
+});
+
+// express server with live reload
+gulp.task('serve', function() {
+    gulp.src('./')
+        .pipe(plugins.webserver({
+            fallback: 'index.html',
+            livereload: true,
+            directoryListing: false,
+            open: true
+        }));
+});
