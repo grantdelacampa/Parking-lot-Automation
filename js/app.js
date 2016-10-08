f1c8e579d63d5687a0a178b23172e277cc8866f5
Jacob <jacob.voronin@gmail.com>
2016-10-07 17:44:43 -0700

    Set up AngularJS and ngRoute #1

---
+++ js/app.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+    angular
+        .module(
+            'parkingLotAutomationApp', // Name of the app
+            [ // Dependencies
+                'ngRoute'
+            ]
+        );
+})();
