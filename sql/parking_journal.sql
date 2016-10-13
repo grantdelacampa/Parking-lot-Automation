c1f3bebdc8ec94a43a00b5ed34b7bada0e18649f
grantdelacampa <grantdelacampa@gmail.com>
2016-10-13 01:09:25 -0700

    Some of the check statements are most likely redundant of course its all a work in progress.

--- sql/parking_journal.sql
+++ sql/parking_journal.sql
@@ -1,1 +1,8 @@
 --should contain [ts_start, ts_end, user_phone_number, money]
+CREATE TABLE parking_journal
+{
+ts_start TIME ,
+ts_end TIME ,
+user_phone_number INT NOT NUll CHECK (LENGTH(user_phone_number)>=1000000000),
+money SMALLINT ,
+}
--- sql/user_table.sql
+++ sql/user_table.sql
@@ -1,1 +1,8 @@
 --should contain [phone_number, qr_code, password]
+create table user_table
+{
+phone_number SMALLINT,
+gr_code
+password int,
+CHECK (LENGTH(password)>8)
+}
