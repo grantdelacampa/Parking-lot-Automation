5170626df9c8cce2ec028dda1861b29652dd8cfb
Jacob <jacob.voronin@gmail.com>
2016-10-18 11:41:00 -0700

    QR Code

--- js/controller.js
+++ js/controller.js
@@ -6,8 +6,22 @@
         )
         .controller(
             'homeController',
-            function () {
+            function ($scope) {
 
+            }
+        )
+        .controller(
+            'qrCodeController',
+            function ($scope) {
+                $scope.buildQRCode = function () {
+                    var qrcode = new QRCode(document.getElementById('qr-code'), {
+                        width : 100,
+                        height : 100
+                    });
+                    qrcode.makeCode('2DE3FF482F');
+                };
+
+                $scope.buildQRCode();
             }
         );
 })();
--- sql/parking_journal.sql
+++ sql/parking_journal.sql
@@ -1,8 +1,7 @@
 --should contain [ts_start, ts_end, user_phone_number, money]
-CREATE TABLE parking_journal
-{
-ts_start TIME ,
-ts_end TIME ,
+CREATE TABLE `parking_journal`(
+    `ts_start` DATE NOT NULL,
+    `ts_end` TIME ,
 user_phone_number INT NOT NUll CHECK (LENGTH(user_phone_number)>=1000000000),
 money SMALLINT ,
-}
+)
