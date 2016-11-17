2ac9ccaf9d564fda7a75200f20d623fb7806c82c
Jacob <jacob.voronin@gmail.com>
2016-11-17 10:03:13 -0800

    Log-in #24

--- js/app.js
+++ js/app.js
@@ -1,56 +1,57 @@
-(function () {
-    'use strict';
-    angular
-        .module(
-            'parkingLotAutomationApp', // Name of the app
-            [ // Dependencies
-                'ui.router'
-            ]
-        )
-        .config(function ($httpProvider) {
-            //Reset headers to avoid OPTIONS request (aka preflight)
-            $httpProvider.defaults.headers.common = {};
-            $httpProvider.defaults.headers.post = {};
-            $httpProvider.defaults.headers.put = {};
-            $httpProvider.defaults.headers.patch = {};
-        })
-        .run(function ($rootScope, $state, $http) {
-            $rootScope.session = null;
-            /*$rootScope.$on('$stateChangeStart',
-                function(event, toState, toParams, fromState, fromParams, options){
-                    console.log($rootScope.session);
-                    //console.log(fromState);
-                    console.log(toState);
-                    //console.log(event);
-                    if(!$rootScope.session && (toState.name != 'log-in' /!*|| toState.name != 'sign-up'*!/)){
-                        event.preventDefault();
-                        console.log('asdf');
-                        $state.go('log-in');
-                    //$rootScope.checkSession(fromState, toState);
-                    // transitionTo() promise will be rejected with
-                    // a 'transition prevented' error
-                    }
-                });*/
-            //$rootScope.checkSession = function (fromState, toState) {
-                var request = {
-                    method: 'POST',
-                    url: 'http://api.localhost/json/response.php',
-                    data: {
-                        request: 'check_session'
-                    }
-                };
-                $http(request).then(
-                    function (response) {
-                        //if(response.data.type == 'error' && fromState.name != 'log-in')
-                            //$state.go('log-in');
-                        console.log(response.data);
-                        // console.log(fromState);
-                        // console.log(toState);
-                        //$state.go('log-in');
-                    }, function (error) {
-                        console.log(error);
-                    }
-                );
-            //};
-        });
-})();
+'use strict';
+var config = {
+    api: 'http://csc131.slavikf.com/api/json/response.php'
+};
+angular
+    .module(
+        'parkingLotAutomationApp', // Name of the app
+        [ // Dependencies
+            'ui.router'
+        ]
+    )
+    .config(function ($httpProvider) {
+        //Reset headers to avoid OPTIONS request (aka preflight)
+        $httpProvider.defaults.headers.common = {};
+        $httpProvider.defaults.headers.post = {};
+        $httpProvider.defaults.headers.put = {};
+        $httpProvider.defaults.headers.patch = {};
+    })
+    .run(function ($rootScope, $state, $http) {
+        $rootScope.session = null;
+        /*$rootScope.$on('$stateChangeStart',
+         function(event, toState, toParams, fromState, fromParams, options){
+         console.log($rootScope.session);
+         //console.log(fromState);
+         console.log(toState);
+         //console.log(event);
+         if(!$rootScope.session && (toState.name != 'log-in' /!*|| toState.name != 'sign-up'*!/)){
+         event.preventDefault();
+         console.log('asdf');
+         $state.go('log-in');
+         //$rootScope.checkSession(fromState, toState);
+         // transitionTo() promise will be rejected with
+         // a 'transition prevented' error
+         }
+         });*/
+        //$rootScope.checkSession = function (fromState, toState) {
+        var request = {
+            method: 'POST',
+            url: config.api,
+            data: {
+                request: 'check_session'
+            }
+        };
+        $http(request).then(
+            function (response) {
+                //if(response.data.type == 'error' && fromState.name != 'log-in')
+                //$state.go('log-in');
+                console.log(response.data);
+                // console.log(fromState);
+                // console.log(toState);
+                //$state.go('log-in');
+            }, function (error) {
+                console.log(error);
+            }
+        );
+        //};
+    });
--- js/controllers/logInController.js
+++ js/controllers/logInController.js
@@ -1,35 +1,33 @@
-(function () {
-    'use strict';
-    angular
-        .module(
-            'parkingLotAutomationApp' // Name of the app
-        )
-        .controller(
-            'logInController',
-            function ($rootScope, $scope, $http) {
-                $scope.logIn = function () {
-                    var request = {
-                        method: 'POST',
-                        url: 'http://api.localhost/json/response.php',
+'use strict';
+angular
+    .module(
+        'parkingLotAutomationApp' // Name of the app
+    )
+    .controller(
+        'logInController',
+        function ($rootScope, $scope, $http) {
+            $scope.logIn = function () {
+                var request = {
+                    method: 'POST',
+                    url: config.api,
+                    data: {
+                        request: 'log_in',
                         data: {
-                            request: 'log_in',
-                            data: {
-                                'telephone': $scope.logIn.telephone,
-                                'password': $scope.logIn.password
-                            }
+                            'telephone': $scope.logIn.telephone,
+                            'password': $scope.logIn.password
                         }
-                    };
-                    $http(request).then(
-                        function (response) {
-                            console.log(response);
-                            if (response.data.type == 'success') {
-                                $rootScope.session = response.data.session;
-                            }
-                        }, function (error) {
-                            console.log(error);
-                        }
-                    )
+                    }
                 };
-            }
-        );
-})();
+                $http(request).then(
+                    function (response) {
+                        console.log(response);
+                        if (response.data.type == 'success') {
+                            $rootScope.session = response.data.session;
+                        }
+                    }, function (error) {
+                        console.log(error);
+                    }
+                )
+            };
+        }
+    );
