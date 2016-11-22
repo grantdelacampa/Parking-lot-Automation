bf891e456698c7c8ee17ad13e7fd79526a395121
Jacob <jacob.voronin@gmail.com>
2016-11-22 12:20:03 -0800

    QR end-point - backend #39 pseudo-code

--- php/parkingFunctions.php
+++ php/parkingFunctions.php
@@ -2,5 +2,28 @@
 
 function clickQRCode($data)
 {
+    $userQRCode = $data->qr_code;
+
+    //run DB query SELECT * FROM `p_spot` WHERE qr_code = $userQRCode;
+    //
+    //if rows.count > 0
+    //
+    //  is_taken = false;
+    //  qr_code = null;
+    //  p_journal -> all records with this qr_code
+    //      if any have ts_end == null
+    //          ts_end = date
+    //          calc Money -> put into `money`
+    //else
+    //
+    //  look for closest !taken spot (SORT BY)
+    //  if all is taken -> error
+    //  else use that closest spot
+    //      is taken = true
+    //      qr_code =  $userQRCode
+    //      p_journal
+    //          qr code
+    //          ts_start
+
     return 'clickQRCode()';
 }
