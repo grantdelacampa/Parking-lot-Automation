2b08d606637256fcd519ceba0b86be0004afbaca
Jacob <jacob.voronin@gmail.com>
2016-12-08 11:20:36 -0800

    Redirect to status after scan

--- js/controllers/qrCodeController.js
+++ js/controllers/qrCodeController.js
@@ -50,7 +50,7 @@ angular
             $scope.startTimer = function () {
                 if (!angular.isDefined(timer))
                     timer = $interval(function () {
-                        $rootScope.checkParkingStatus();
+                        $rootScope.checkParkingStatus(true);
                     }, 5000);
             };
 
@@ -60,7 +60,7 @@ angular
             });
 
             $scope.buildQRCode();
-            $rootScope.checkParkingStatus();
+            $rootScope.checkParkingStatus(true);
             $scope.startTimer();
         }
     );
